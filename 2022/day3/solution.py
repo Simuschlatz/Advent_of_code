1ce8ad1888222a267d05caae30aff2cc3e8687b8
Simon Ma <simon.mama07@gmail.com>
2022-12-04 10:46:05 +0100

    shorter version

--- 2022/day3/solution.py
+++ 2022/day3/solution.py
@@ -3,9 +3,9 @@ with open("input.txt", "r") as file:
     data =[line.rstrip() for line in data]
 priority = lambda ascii, char: ascii - 96 if char.islower() else ascii - 38
 def part_one():
-    data = [(set(line[:len(line)//2]) & set(line[len(line)//2:])).pop() for line in data]
+    intersections = [(set(line[:len(line)//2]) & set(line[len(line)//2:])).pop() for line in data]
     total = 0
-    for intersection in data:
+    for intersection in intersections:
         total += priority(ord(intersection), intersection)
     return total
 def part_two():
@@ -13,12 +13,10 @@ def part_two():
         l = len(iterable)
         for ndx in range(0, l, n):
             yield iterable[ndx:min(ndx + n, l)]
+    lines = [set(line) for line in data]
     total = 0
-    for group in batch(data):
-        common = set(group[0])
-        for i in range(1, 3):
-            common &= set(group[i])
-        common = common.pop()
+    for group in batch(lines):
+        common = (group[0] & group[1] & group[2]).pop()
         total += priority(ord(common), common)
     return total
 print(f"part one: {part_one()}")  
